7d3224c3437d4663ee99c25fcc2447f6bc1d208b
Nathan Campos <eeepc904@gmail.com>
2013-01-26 23:41:42 -0200

    Indexer is 'done'

--- indexer.php
+++ indexer.php
@@ -10,7 +10,33 @@ function build_repo_url($repository, $category) {
 	return "$repo_base_url/$repository/feed/$category/Packages";
 }
 
-print $repo_url;
+function parse_packages($file) {
+	// Gets an array with raw plain text from each package description.
+	$package_arr = explode("\n\n", $file);
 
+	// Pops the last element if its empty.
+	if (empty($package_arr[count($package_arr) - 1])) {
+		array_pop($package_arr);
+	}
+	
+	// Parse each package description.
+	for ($i = 0; $i < count($package_arr); $i++) {
+		$curr_package = explode("\n", $package_arr[$i]);
+		$package_arr[$i] = array();
+
+		for ($j = 0; $j < count($curr_package); $j++) {
+			$desc_arr = explode(": ", $curr_package[$j], 2);
+			$package_arr[$i][$desc_arr[0]] = $desc_arr[1];
+		}
+	}
+	
+	return $package_arr;
+}
+
+$packages_file = file_get_contents($repo_url);
+//header("Content-Type: text/plain");
+header("Content-Type: application/json");
+echo json_encode(parse_packages($packages_file));
+//print_r(parse_packages($packages_file));
 
 ?>
